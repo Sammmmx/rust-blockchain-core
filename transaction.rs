7cfb90422ce4ca7a41b38a7233235e2094f55025
sammmmx <forsamsworkrelated@gmail.com>
2026-04-20 15:49:13 +0530

    Blockchain componenets

---
+++ transaction.rs
@@ -0,0 +1,26 @@
+use sha2::{Sha256, Digest};
+
+pub type Address = String;
+
+#[derive(Clone, Debug)]
+pub struct Transaction {
+    pub from: Address,
+    pub to: Address,
+    pub amount: u64,
+    pub nonce: u64,
+}
+
+impl Transaction {
+    pub fn new(from: Address, to: Address, amount: u64, nonce: u64) -> Self {
+        Self { from, to, amount, nonce }
+    }
+
+    pub fn hash(&self) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(format!(
+            "{}{}{}{}",
+            self.from, self.to, self.amount, self.nonce
+        ));
+        format!("{:x}", hasher.finalize())
+    }
+}
